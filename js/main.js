fecd35c4ca6c5516435f9ff34530c42e1965de6a
Geoffrey DAHURON <geoffreyd21@hotmail.fr>
2017-10-21 19:38:28 +0200

    Nav + présentation + début partie compétences

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ $(function(){
         interval: 5000 //changes the speed
     });
 })
+
+//Textillate (animation) pour le texte dans le nav
+$(function(){
+    $('.textillate').textillate({in: {effect: 'fadeIn'}});
+    
+})^
+
+//Animation competences
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},3000);
+	});
+});
