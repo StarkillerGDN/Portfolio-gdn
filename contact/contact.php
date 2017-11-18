b7a11b75fd7c4508f2518404551973a1f5e942af
Geoffrey DAHURON <geoffreyd21@hotmail.fr>
2017-11-18 12:02:02 +0100

    TEST PHP MAILER

--- contact/contact.php
+++ contact/contact.php
@@ -26,7 +26,7 @@
     require '../vendor/autoload.php';
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $mail = new PHPMailer(false);                              // TRUE = activer les exceptions
+        $mail = new PHPMailer(false);                             // TRUE = activer les exceptions
         //Server settings
         $mail->SMTPDebug = 0;                                 //Desactiver le Debug pour la prod
         $mail->isSMTP();                                      //Force PHP Mailer à utiliser SMTP
@@ -36,19 +36,33 @@
         $mail->Password = '35JCXtUEPeUE';                           // Password
         $mail->SMTPSecure = 'tls';                            // TLS encrypte
         $mail->Port = 587;                                    // Port TLS
+        
+        //Récupération des champs et vérifs contre les injections
+        $nomPrenom = test_donnees($_POST["nomPrenom"]);
+        $mail = test_donnees($_POST["mail"]);
+        $message = test_donnees($_POST["message"]);
+
+        //Entête
+        $mail->setFrom('noreply@geogeoquidev.fr', 'GeogeoROBOT');
+        $mail->addAddress('geoffrey211295@gmail.com', 'Geoffrey DAHURON');    
+        $mail->addReplyTo('noreply@geogeoquidev.fr', 'GeogeoROBOT');
     
-        //Recipients
-        $mail->setFrom('geoffrey211295@gmail.com', 'GeogeoROBOT');
-        $mail->addAddress('geoffreyd21@hotmail.fr', 'Joe User');    
-        $mail->addReplyTo('info@example.com', 'Information');
     
-    
-        //Content
+        //Contenu
         $mail->isHTML(true);                                  // Email au format HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-    
+        $mail->Subject = 'Mail envoyer depuis geogeoquidev.fr';
+        $mail->Body    = 'Vous avez reçu un message.<br><br>Voici le détail :<br>- Nom et Prénom : $nomPrenom <br>- E-mail : $mail <br>- Message : <br>$message';
+        $mail->AltBody = 'Vous avez reçu un message.\r\n\r\nVoici le détail :\r\n- Nom et Prénom : $nomPrenom\r\n- E-mail : $mail\r\n- Message :\r\n$message';
+        
+        //Envoi du mail
         $mail->send();
+    }
+
+     //Fonction de vérification des champs contre les hacks
+     function test_donnees($data){
+        $data = trim($data);
+        $data = stripcslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
 ?>
