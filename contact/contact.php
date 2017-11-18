6a00f3f96271cff5c17bc88ec70929e1277abba3
Geoffrey DAHURON <geoffreyd21@hotmail.fr>
2017-11-18 11:29:41 +0100

    TEST PHP MAILER

--- contact/contact.php
+++ contact/contact.php
@@ -23,17 +23,17 @@
     } */
 
 
-    require '/vendor/autoload.php';
+    require '../vendor/autoload.php';
 
     $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
     try {
         //Server settings
         $mail->SMTPDebug = 2;                                 // Enable verbose debug output
         $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp.sfr.fr';  // Specify main and backup SMTP servers
+        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
         $mail->Username = 'geoffrey211295@gmail.com';                 // SMTP username
-        $mail->Password = 'Masseffect3';                           // SMTP password
+        $mail->Password = '35JCXtUEPeUE';                           // SMTP password
         $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 587;                                    // TCP port to connect to
     
