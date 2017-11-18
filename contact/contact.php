e275e3d1d3c1682336a3e2d10c4d49d5475be329
Geoffrey DAHURON <geoffreyd21@hotmail.fr>
2017-11-18 12:28:20 +0100

    Inplémentation finale PHP mailer

--- contact/contact.php
+++ contact/contact.php
@@ -1,30 +1,8 @@
 <?php
-    //Vérification que la requête est POST
-   /* if($_SERVER["REQUEST_METHOD"] == "POST"){
-        //Récupération des champs et vérifs contre les injections
-        $nomPrenom = test_donnees($_POST["nomPrenom"]);
-        $mail = test_donnees($_POST["mail"]);
-        $message = test_donnees($_POST["message"]);
-        //Création du mail
-        $to = 'geoffrey211295@gmail.com';
-        $sujet = "Mail envoyer depuis geogeoquidev.fr";
-        $corps_message = "Vous avez reçu un message.\r\n\r\nVoici le détail :\r\n- Nom et Prénom : $nomPrenom\r\n- E-mail : $mail\r\n- Message :\r\n$message";
-        $header = "From : \"GeogeoLeRobot\"<noreply@geogeoquidev.fr>\r\n";
-        $header.= "Reply-to : \"GeogeoLeRobot\"<noreply@geogeoquidev.fr>\r\n";
-        //Envoi du mail
-        mail($to, $sujet, $corps_message, $header);
-    }
-    //Fonction de vérification des champs contre les hacks
-    function test_donnees($data){
-        $data = trim($data);
-        $data = stripcslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    } */
-
     use PHPMailer\PHPMailer\PHPMailer;
     require '../vendor/autoload.php';
 
+    //Vérification que la requête est POST
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         //Récupération des champs et vérifs contre les injections
         $nomPrenom = test_donnees($_POST["nomPrenom"]);
@@ -51,7 +29,6 @@
         $mail->isHTML(true);                                  // Email au format HTML
         $mail->Subject = 'Mail envoyer depuis geogeoquidev.fr';
         $mail->Body    = 'Vous avez reçu un message.<br><br>Voici le détail :<br>- Nom et Prénom : ' .$nomPrenom.'<br>- E-mail : '  .$email.'<br>- Message : <br>' .$message;
-        $mail->AltBody = 'Vous avez reçu un message.\r\n\r\nVoici le détail :\r\n- Nom et Prénom : $nomPrenom\r\n- E-mail : $email\r\n- Message :\r\n$message';
         
         //Envoi du mail
         $mail->send();
