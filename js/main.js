4b2ac1905967430f1bcd1baea4f090df0ecdc5d6
Geoffrey DAHURON <geoffreyd21@hotmail.fr>
2017-11-22 13:23:04 +0100

    Changement couleurs sites + suppression carousel + Ajout section Projets

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-// Script to Activate the Carousel
-$(function(){
-    $('.carousel').carousel({
-        interval: 5000 //changes the speed
-    });
-})
-
 //Textillate (animation) pour le texte dans le nav
 $(function(){
     $('.textillate').textillate({in: {effect: 'fadeIn'}});
@@ -116,33 +109,33 @@ function validerMail(champs){
 $('#facebook').hover(
     function(){
         //Intrduction du nouvel elem
-        $(this).html(" <svg style='width:40px;height:40px' viewBox='0 0 24 24'><path fill='#000000' d='M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z' /></svg>");
+        $(this).html(" <svg style='width:40px;height:40px' viewBox='0 0 24 24'><path fill='	#8b9dc3' d='M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z' /></svg>");
     },
     //Quand on quitte le hover
     function(){
-        $(this).html(" <svg style='width:40px;height:40px' viewBox='0 0 24 24'><path fill='#56739A' d='M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z' /></svg>")
+        $(this).html(" <svg style='width:40px;height:40px' viewBox='0 0 24 24'><path fill='#3b5998' d='M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z' /></svg>")
     }
 );
 
 $('#linkedin').hover(
     function(){
         //Intrduction du nouvel elem
-        $(this).html("<svg style='width:40px;height:40px' viewBox='0 0 24 24'><path fill='#000000' d='M19,3A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3H19M18.5,18.5V13.2A3.26,3.26 0 0,0 15.24,9.94C14.39,9.94 13.4,10.46 12.92,11.24V10.13H10.13V18.5H12.92V13.57C12.92,12.8 13.54,12.17 14.31,12.17A1.4,1.4 0 0,1 15.71,13.57V18.5H18.5M6.88,8.56A1.68,1.68 0 0,0 8.56,6.88C8.56,5.95 7.81,5.19 6.88,5.19A1.69,1.69 0 0,0 5.19,6.88C5.19,7.81 5.95,8.56 6.88,8.56M8.27,18.5V10.13H5.5V18.5H8.27Z' /></svg>");
+        $(this).html("<svg style='width:40px;height:40px' viewBox='0 0 24 24'><path fill='#00A0DC' d='M19,3A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3H19M18.5,18.5V13.2A3.26,3.26 0 0,0 15.24,9.94C14.39,9.94 13.4,10.46 12.92,11.24V10.13H10.13V18.5H12.92V13.57C12.92,12.8 13.54,12.17 14.31,12.17A1.4,1.4 0 0,1 15.71,13.57V18.5H18.5M6.88,8.56A1.68,1.68 0 0,0 8.56,6.88C8.56,5.95 7.81,5.19 6.88,5.19A1.69,1.69 0 0,0 5.19,6.88C5.19,7.81 5.95,8.56 6.88,8.56M8.27,18.5V10.13H5.5V18.5H8.27Z' /></svg>");
     },
     //Quand on quitte le hover
     function(){
-        $(this).html("<svg style='width:40px;height:40px' viewBox='0 0 24 24'><path fill='#56739A' d='M19,3A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3H19M18.5,18.5V13.2A3.26,3.26 0 0,0 15.24,9.94C14.39,9.94 13.4,10.46 12.92,11.24V10.13H10.13V18.5H12.92V13.57C12.92,12.8 13.54,12.17 14.31,12.17A1.4,1.4 0 0,1 15.71,13.57V18.5H18.5M6.88,8.56A1.68,1.68 0 0,0 8.56,6.88C8.56,5.95 7.81,5.19 6.88,5.19A1.69,1.69 0 0,0 5.19,6.88C5.19,7.81 5.95,8.56 6.88,8.56M8.27,18.5V10.13H5.5V18.5H8.27Z' /></svg>");
+        $(this).html("<svg style='width:40px;height:40px' viewBox='0 0 24 24'><path fill='#007bb6' d='M19,3A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3H19M18.5,18.5V13.2A3.26,3.26 0 0,0 15.24,9.94C14.39,9.94 13.4,10.46 12.92,11.24V10.13H10.13V18.5H12.92V13.57C12.92,12.8 13.54,12.17 14.31,12.17A1.4,1.4 0 0,1 15.71,13.57V18.5H18.5M6.88,8.56A1.68,1.68 0 0,0 8.56,6.88C8.56,5.95 7.81,5.19 6.88,5.19A1.69,1.69 0 0,0 5.19,6.88C5.19,7.81 5.95,8.56 6.88,8.56M8.27,18.5V10.13H5.5V18.5H8.27Z' /></svg>");
     }
 );
 
 $('#gmail').hover(
     function(){
         //Intrduction du nouvel elem
-        $(this).html("<svg style='width:40px;height:40px' viewBox='0 0 24 24'><path fill='#000000' d='M20,18H18V9.25L12,13L6,9.25V18H4V6H5.2L12,10.25L18.8,6H20M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z' /></svg>");
+        $(this).html("<svg style='width:40px;height:40px' viewBox='0 0 24 24'><path fill='#ff5d5d' d='M20,18H18V9.25L12,13L6,9.25V18H4V6H5.2L12,10.25L18.8,6H20M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z' /></svg>");
     },
     //Quand on quitte le hover
     function(){
-        $(this).html("<svg style='width:40px;height:40px' viewBox='0 0 24 24'><path fill='#56739A' d='M20,18H18V9.25L12,13L6,9.25V18H4V6H5.2L12,10.25L18.8,6H20M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z' /></svg>");
+        $(this).html("<svg style='width:40px;height:40px' viewBox='0 0 24 24'><path fill='#ff4343' d='M20,18H18V9.25L12,13L6,9.25V18H4V6H5.2L12,10.25L18.8,6H20M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z' /></svg>");
     }
 );
 
@@ -160,15 +153,3 @@ $(document).ready(
 
     }
 );
-
-//Swipe du carousel
-$(function(){
-    $('.carousel').swipe({
-        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-            //Récupération 
-            if (direction == 'left') $(this).carousel('next');
-            if (direction == 'right') $(this).carousel('prev');  
-        },
-        allowPageScroll:"vertical"
-    });
-});
